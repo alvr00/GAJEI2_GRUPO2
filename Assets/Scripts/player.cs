e907190cc6b2ce84d467159040f664b2f328f4cd
alvr00 <alvaro9rodrigo@gmail.com>
2021-04-13 21:18:18 +0200

    saltos y colision misil

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    float velocidad = 5f;
+    float velocidad = 8f;
+    [SerializeField] GameObject misilPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,6 +18,18 @@ public class player : MonoBehaviour
     {
         float xPos = Input.GetAxisRaw("Horizontal");
         float yPos = Input.GetAxisRaw("Vertical");
-        transform.Translate(new Vector3(xPos, yPos,0).normalized * velocidad * Time.deltaTime );
+        transform.Translate(new Vector3(xPos, yPos, 0).normalized * velocidad * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("misilBoba1"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
