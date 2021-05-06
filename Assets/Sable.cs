f10d9b80ced2da0c8ffb0af0062b67c9fc43cf38
TorresVic <victortorresano28@gmail.com>
2021-05-06 18:51:56 +0200

    anim espada/ movimiento espada

---
+++ Assets/Sable.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sable : MonoBehaviour
+{
+
+    Vector3 objetivo;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        objetivo = player.transform.position + new Vector3(5, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != objetivo)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, objetivo, 10 * Time.deltaTime);
+        }
+        else
+        {
+            objetivo = player.transform.position;
+
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerGirl.cs
+++ Assets/Scripts/PlayerGirl.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class PlayerGirl : MonoBehaviour
 {
+
     float velocidad = 8f;
     [SerializeField] GameObject misilPrefab;
     [SerializeField] Animator anim;
     [SerializeField] GameObject sableVolandoPrefab;
-    
-    
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,24 +22,32 @@ public class player : MonoBehaviour
     void Update()
     {
         float xPos = Input.GetAxisRaw("Horizontal");
-        
+
         transform.Translate(new Vector3(xPos, 0, 0).normalized * velocidad * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-            anim.SetTrigger("jump");
+            anim.SetTrigger("saltar");
 
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            LanzarSable();
         }
 
     }
-   
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("misilBoba1"))
         {
             Destroy(collision.gameObject);
         }
+    }
+    void LanzarSable()
+    {
+        Instantiate(sableVolandoPrefab, transform.position + new Vector3(1, 0, 0), Quaternion.identity);
     }
 }
